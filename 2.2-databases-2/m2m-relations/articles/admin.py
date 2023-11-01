f8a4776b403113a3f063dfe893e536fbd31248c3
Aserold <oluwoartem@gmail.com>
2023-11-01 16:39:58 +0300

    orm2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Scope, Tag
 
-from .models import Article
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count_main = 0
+        for form in self.forms:
+            if form.is_valid() and form.cleaned_data.get('is_main', False):
+                count_main += 1
+
+        if count_main != 1:
+            raise ValidationError('Должен быть выбран один и только один основной раздел.')
+        
+        return super().clean()
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
+class ObjectAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
+
+
+@admin.register(Tag)
+class ObjectAdmin(admin.ModelAdmin):
     pass
